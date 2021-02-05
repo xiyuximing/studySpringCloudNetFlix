de004d4dbae002ec62b11402177cd2d0fa05bfd9
xiyuximing <40282978+xiyuximing@users.noreply.github.com>
2021-02-05 17:49:30 +0800

    更改提交

---
+++ SpringcloudnetflixDemoParent/springcloudnetflix-demo-gateway-9002/src/main/java/com/cy/spcdemo/SpringCloudNetflixGateWay9002.java
@@ -0,0 +1,13 @@
+package com.cy.spcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class SpringCloudNetflixGateWay9002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudNetflixGateWay9002.class, args);
+    }
+
+}
---
+++ SpringcloudnetflixDemoParent/springcloudnetflix-demo-gateway-9002/src/main/java/com/cy/spcdemo/fliter/BlastRequestFilter.java
@@ -0,0 +1,4 @@
+package com.cy.spcdemo.fliter;
+
+public class BlastRequestFilter {
+}
